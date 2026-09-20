61e1b8f33110ddf88120da20a9616c06740ebceb
Ayman Hazem <aymooongsr2@gmail.com>
2026-09-20 18:00:21 +0300

    Initial commit (seed)

---
+++ src/test/java/com/ayman/TimelinePreviewSystem/TimelinePreviewSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayman.TimelinePreviewSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelinePreviewSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
